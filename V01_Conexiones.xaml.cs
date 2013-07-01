3255be478fb8f30ed4ead5c080f73e31c6418bab
ccallado <ccallado2005@gmail.com>
2013-07-01 20:57:07 +0200

    ADO Conectado
    Conexión por oledb a una base de datos de Access.

---
+++ V01_Conexiones.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoDatos
+{
+    /// <summary>
+    /// Lógica de interacción para V01_Conexiones.xaml
+    /// </summary>
+    public partial class V01_Conexiones : Window
+    {
+        System.Data.OleDb.OleDbConnection cnnAccess;
+
+        public V01_Conexiones()
+        {
+            InitializeComponent();
+            cnnAccess = new System.Data.OleDb.OleDbConnection();
+            //Con el caracter @"\n" quita los caracteres de escape de la cadena
+            cnnAccess.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Formacion\\Desktop\SQL\Northwind.mdb";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //solo se pueden usar los estados Open o Closed
+            if (cnnAccess.State == System.Data.ConnectionState.Closed)
+            {
+                cnnAccess.Open();
+                MessageBox.Show("Abierta");
+            }
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            cnnAccess.Close();
+            MessageBox.Show("Cerrada");
+        }
+    }
+}
