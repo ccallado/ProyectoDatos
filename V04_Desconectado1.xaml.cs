84a14cdb4eadee496812d3c9d4d6c1e17810fefd
ccallado <ccallado2005@gmail.com>
2013-07-05 19:01:52 +0200

    Terminado Entorno Desconectado

--- V04_Desconectado1.xaml.cs
+++ V04_Desconectado1.xaml.cs
@@ -18,7 +18,7 @@ namespace ProyectoDatos
     /// </summary>
     public partial class V04_Desconectado1 : Window
     {
-        NWDataSet.ProductsDataTable ProductosFiltrados = null ;
+        NWDataSet.ProductsDataTable ProductosFiltrados = null;
 
         public V04_Desconectado1()
         {
@@ -126,10 +126,147 @@ namespace ProyectoDatos
             {
                 NWDataSetTableAdapters.ProductsTableAdapter taProd;
                 taProd = new NWDataSetTableAdapters.ProductsTableAdapter();
-                taProd.Update(tabla);
-                //Otra manera pero con cash
-                //taProd.Update((NWDataSet.ProductsDataTable)dataGrid1.ItemsSource);
-                MessageBox.Show("BBDD Actualizada.");
+                try
+                {
+                    taProd.Update(tabla);
+                    //Otra manera pero con cash
+                    //taProd.Update((NWDataSet.ProductsDataTable)dataGrid1.ItemsSource);
+                    MessageBox.Show("BBDD Actualizada.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Operaciónn abortada:\n" + ex.Message);
+                }
+            }
+        }
+
+        //Actualizar con Transacción
+        //Añadimos la referencia a la librería está en .NET (System.Transactions)
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            NWDataSet.ProductsDataTable tabla;
+            tabla = dataGrid1.ItemsSource as NWDataSet.ProductsDataTable;
+            if (tabla == null)
+            {
+                MessageBox.Show("Muestre los productos para actualizarlos...");
+            }
+            else
+            {
+                NWDataSetTableAdapters.ProductsTableAdapter taProd;
+                taProd = new NWDataSetTableAdapters.ProductsTableAdapter();
+
+                //Transacción
+
+                //otro metodo
+                //System.Data.SqlClient.SqlConnection cnn;
+                //using (cnn = 
+                //    new System.Data.SqlClient.SqlConnection;
+                //using (System.Data.SqlClient.SqlConnection cnn = 
+                //    new System.Data.SqlClient.SqlConnection;
+
+                using (System.Transactions.TransactionScope tr =
+                    new System.Transactions.TransactionScope())
+                {
+                    //Dentro hay que llamar al método .Complete()
+                    //Sino deshace todo lo que le hayamos dicho
+
+                    try
+                    {
+                        taProd.Update(tabla);
+                        tr.Complete();
+                        MessageBox.Show("BBDD Actualizada.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Operaciónn abortada:\n" + ex.Message);
+                    }
+                }
+            }
+
+        }
+
+        //Actualizar sin parada
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            NWDataSet.ProductsDataTable tabla;
+            tabla = dataGrid1.ItemsSource as NWDataSet.ProductsDataTable;
+            if (tabla == null)
+            {
+                MessageBox.Show("Muestre los productos para actualizarlos...");
+            }
+            else
+            {
+                NWDataSetTableAdapters.ProductsTableAdapter taProd;
+                taProd = new NWDataSetTableAdapters.ProductsTableAdapter();
+                //Decide si cuando ocurre un error para o continúa con el siguiente
+                //Con Insert, Delete o Update
+                taProd.Adapter.ContinueUpdateOnError = true;
+
+                try
+                {
+                    taProd.Update(tabla);
+                    //Si no tiene errores
+                    if (!tabla.HasErrors)
+                        MessageBox.Show("BBDD Actualizada.");
+                    else
+                    {
+                        string cad = "";
+                        foreach (NWDataSet.ProductsRow prod in tabla.GetErrors())
+                        {
+                            //Error al acceder a registros borrados no puedo saber su contenido
+                            //cad += prod.ProductID + " - " + prod.ProductName +
+                            cad +=
+                                (prod.RowState != System.Data.DataRowState.Deleted ?
+                                    prod.ProductID + " - " + prod.ProductName :
+                                    "Registro borrado.")
+                                + "\nEstado: " + prod.RowState +
+                                "\n" + prod.RowError + "\n\n";
+                        }
+
+                        MessageBoxResult res;
+                        res = MessageBox.Show(cad + "\n¿Quieres deshacer los cambios pendientes?",
+                            "BBDD Actualizada CON errores.",
+                            MessageBoxButton.YesNo,
+                            MessageBoxImage.Error,
+                            MessageBoxResult.No);
+                        if (res == MessageBoxResult.Yes)
+                            //Deshacer los cambios 
+                            tabla.RejectChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Operaciónn abortada:\n" + ex.Message);
+                }
+            }
+        }
+
+        //Filtro (Vista)
+        System.Data.DataView vistaProductos = null;
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            //Instancio la vista si no lo está
+            if (dataGrid1.ItemsSource != null)
+            {
+                if (vistaProductos == null)
+                {
+                    vistaProductos = new System.Data.DataView();
+                    vistaProductos.Table = (NWDataSet.ProductsDataTable)dataGrid1.ItemsSource;
+                }
+
+                int IdCategoria;
+
+                if (int.TryParse(textBox1.Text, out IdCategoria))
+                    vistaProductos.RowFilter = "CategoryID = " + IdCategoria;
+                else
+                {
+                    vistaProductos.RowFilter = "";
+                    textBox1.Text = "";
+                }
+
+                //Asigno la vista al datagrid
+                dataGrid1.ItemsSource = vistaProductos;
             }
         }
     }
